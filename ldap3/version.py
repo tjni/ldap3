3236989337def8ccafb6cc283dc6510fc827e73e
Theodore Ni <3806110+tjni@users.noreply.github.com>
2025-01-24 11:13:21 -0800

    Update version and URL for 2.9.1.1.

--- ldap3/version.py
+++ ldap3/version.py
@@ -3,10 +3,10 @@
 # on system uname_result(system='Windows', node='ELITE10GC', release='10', version='10.0.19041', machine='AMD64')
 # with Python 3.9.1 - ('tags/v3.9.1:1e5d33e', 'Dec  7 2020 17:08:21') - MSC v.1927 64 bit (AMD64)
 #
-__version__ = '2.9'
+__version__ = '2.9.1.1'
 __author__ = 'Giovanni Cannata'
 __email__ = 'cannatag@gmail.com'
-__url__ = 'https://github.com/cannatag/ldap3'
+__url__ = 'https://github.com/tjni/ldap3'
 __description__ = 'A strictly RFC 4510 conforming LDAP V3 pure Python client library'
 __status__ = '5 - Production/Stable'
 __license__ = 'LGPL v3'
